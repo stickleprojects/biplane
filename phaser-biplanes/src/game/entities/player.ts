31f41097c49c7746d8bfd6ba6cc979c3cbdd2aa8
Kieron Wray <kieron.wray@gmail.com>
2025-06-24 18:18:43 +0100

    moved player logic into the player entity added some instructions

--- phaser-biplanes/src/game/entities/player.ts
+++ phaser-biplanes/src/game/entities/player.ts
@@ -1,4 +1,4 @@
-import Phaser from "phaser";
+import Phaser, { Input } from "phaser";
 
 class Player extends Phaser.GameObjects.Sprite {
   constructor(scene: Phaser.Scene, x: number, y: number) {
@@ -11,6 +11,40 @@ class Player extends Phaser.GameObjects.Sprite {
 
     const body = this.body as Phaser.Physics.Arcade.Body;
     body.setAllowGravity(false);
+  }
+
+  update(time: number, delta: number, input: Input.InputPlugin): void {
+    if (
+      input.keyboard?.checkDown(
+        input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT),
+        0
+      )
+    ) {
+      this.setRotation(this.rotation + 0.1);
+    }
+    if (
+      input.keyboard?.checkDown(
+        input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT),
+        0
+      )
+    ) {
+      this.setRotation(this.rotation - 0.1);
+    }
+
+    const speed = 0.2; // Adjust as needed
+    var b = this.body as Phaser.Physics.Arcade.Body;
+    const angle = b.rotation; // Get the angle in degrees
+
+    // Convert angle to radians
+    const rad = Phaser.Math.DegToRad(angle);
+
+    // Calculate velocity components
+    const vx = Math.cos(rad) * speed;
+    const vy = Math.sin(rad) * speed;
+
+    // Apply velocity to the sprite's body
+    b.setVelocity(vx, vy);
+    this.scene.physics.world.wrap(this, 0);
   }
 }
 
--- phaser-biplanes/src/game/scenes/MainMenu.ts
+++ phaser-biplanes/src/game/scenes/MainMenu.ts
@@ -3,8 +3,8 @@ import Player from "../entities/player";
 
 export class MainMenu extends Scene {
   background: GameObjects.Image;
-  logo: GameObjects.Image;
-  title: GameObjects.Text;
+
+  instructions: GameObjects.Text;
 
   player1: Player;
 
@@ -20,8 +20,6 @@ export class MainMenu extends Scene {
   }
   preload() {}
   create() {
-    //his.background = this.add.image(512, 384, "background");
-
     this.initPhysics();
 
     this.player1 = new Player(this, 300, 200);
@@ -29,40 +27,16 @@ export class MainMenu extends Scene {
     this.input.once("pointerdown", () => {
       this.scene.start("Game");
     });
+
+    this.instructions = this.add.text(
+      100,
+      100,
+      "Use left/right arrows to steer the plane.\nClick to end the game."
+    );
   }
 
   update(time: number, delta: number) {
     this.physics.world.step(delta);
-    if (
-      this.input.keyboard?.checkDown(
-        this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT),
-        0
-      )
-    ) {
-      this.player1.setRotation(this.player1.rotation + 0.1);
-    }
-    if (
-      this.input.keyboard?.checkDown(
-        this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT),
-        0
-      )
-    ) {
-      this.player1.setRotation(this.player1.rotation - 0.1);
-    }
-
-    const speed = 0.2; // Adjust as needed
-    var b = this.player1.body as Phaser.Physics.Arcade.Body;
-    const angle = b.rotation; // Get the angle in degrees
-
-    // Convert angle to radians
-    const rad = Phaser.Math.DegToRad(angle);
-
-    // Calculate velocity components
-    const vx = Math.cos(rad) * speed;
-    const vy = Math.sin(rad) * speed;
-
-    // Apply velocity to the sprite's body
-    b.setVelocity(vx, vy);
-    this.physics.world.wrap(this.player1, 0);
+    this.player1.update(time, delta, this.input);
   }
 }
