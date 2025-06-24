17f1ce9be044c9559438a113ea48a2d751a47f82
Kieron Wray <kieron.wray@gmail.com>
2025-06-24 18:21:42 +0100

    removed mainmenu, moved code into game

--- phaser-biplanes/src/game/scenes/Game.ts
+++ phaser-biplanes/src/game/scenes/Game.ts
@@ -1,35 +1,40 @@
-import { Scene } from 'phaser';
+import { GameObjects, Scene } from "phaser";
+import Player from "../entities/player";
 
-export class Game extends Scene
-{
-    camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
-    msg_text : Phaser.GameObjects.Text;
+export class Game extends Scene {
+  camera: Phaser.Cameras.Scene2D.Camera;
+  background: Phaser.GameObjects.Image;
+  player1: Player;
+  instructions: GameObjects.Text;
 
-    constructor ()
-    {
-        super('Game');
-    }
+  initPhysics() {
+    this.physics.world.setBounds(0, 0, 1024, 768);
+    this.physics.world.setFPS(60);
+    this.physics.world.gravity.set(0, 0);
+    this.physics.world.setBoundsCollision(true, true, true, true);
+  }
+  constructor() {
+    super("Game");
+  }
 
-    create ()
-    {
-        this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
+  create() {
+    this.camera = this.cameras.main;
+    this.camera.setBackgroundColor(0x0000a0);
 
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
+    this.background = this.add.image(512, 384, "background");
+    this.background.setAlpha(0.5);
 
-        this.msg_text = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        });
-        this.msg_text.setOrigin(0.5);
+    this.initPhysics();
 
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('GameOver');
-
-        });
-    }
+    this.instructions = this.add.text(
+      100,
+      100,
+      "Use left/right arrows to steer the plane.\nClick to end the game."
+    );
+    this.player1 = new Player(this, 300, 200);
+  }
+  update(time: number, delta: number) {
+    this.physics.world.step(delta);
+    this.player1.update(time, delta, this.input);
+  }
 }
--- phaser-biplanes/src/game/scenes/MainMenu.ts
+++ phaser-biplanes/src/game/scenes/MainMenu.ts
@@ -1,42 +1,14 @@
-import { Scene, GameObjects } from "phaser";
-import Player from "../entities/player";
+import { Scene } from "phaser";
 
 export class MainMenu extends Scene {
-  background: GameObjects.Image;
-
-  instructions: GameObjects.Text;
-
-  player1: Player;
-
   constructor() {
     super("MainMenu");
   }
 
-  initPhysics() {
-    this.physics.world.setBounds(0, 0, 1024, 768);
-    this.physics.world.setFPS(60);
-    this.physics.world.gravity.set(0, 0);
-    this.physics.world.setBoundsCollision(true, true, true, true);
-  }
   preload() {}
   create() {
-    this.initPhysics();
-
-    this.player1 = new Player(this, 300, 200);
-
     this.input.once("pointerdown", () => {
       this.scene.start("Game");
     });
-
-    this.instructions = this.add.text(
-      100,
-      100,
-      "Use left/right arrows to steer the plane.\nClick to end the game."
-    );
-  }
-
-  update(time: number, delta: number) {
-    this.physics.world.step(delta);
-    this.player1.update(time, delta, this.input);
   }
 }
